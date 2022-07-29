753f0f0f32ca18e307de7dc1d3a582167f7ca8a6
Anton Mihaylov <antonmmihaylov@gmail.com>
2022-07-29 20:08:37 +0300

    comments

--- index.tsx
+++ index.tsx
@@ -1,6 +1,18 @@
 import { Card } from "./components"
+import * as Card2  from "./components/Card"
 import React from "react"
 
 const App = () => {
-  return <Card.Header></Card.Header>
+  return <>
+    {/*
+      Go to declaration or Cmd-click on Header doesn't work.
+      Go to declaration for Card works, but it goes to the components/index.ts
+    */}
+    <Card.Header></Card.Header>
+
+    {/*
+      If imported directly from "components/Card" it works:
+    */}
+    <Card2.Header />
+  </>
 }
